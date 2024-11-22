9437f0b03c302c7d20cbf28f15aa32ddefd48a3d
Marina Paixao <marina.cezario@linx.com.br>
2024-11-21 23:43:00 -0300

    Aula 4: fazendo upload de arquivos via api

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -2,9 +2,18 @@ import conectarAoBanco from "../config/dbConfig.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export default async function getTodosPosts() {
+// Função para pegar todos os elementos
+export async function getTodosPosts() {
   const db = conexao.db("imersao-instabytes");
   const colecao = db.collection("posts");
 
   return colecao.find().toArray();
+}
+
+// Função para inserir um novo elemento
+export async function criarPost(novoPost) {
+  const db = conexao.db("imersao-instabytes");
+  const colecao = db.collection("posts");
+
+  return colecao.insertOne(novoPost);
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,21 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import {
+  enviarPosts,
+  listarPosts,
+  uploadImagem,
+} from "../controllers/postsController.js";
 import { listarUsuarios } from "../controllers/usuariosController.js";
+
+const upload = multer({ dest: "./uploads" });
 
 const routes = (app) => {
   app.use(express.json());
 
   app.get("/posts", listarPosts);
+  app.post("/posts", enviarPosts);
+  app.post("/upload", upload.single("imagem"), uploadImagem);
+
   app.get("/usuarios", listarUsuarios);
 };
 
